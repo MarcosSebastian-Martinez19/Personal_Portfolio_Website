aca300785a862e172ba9643f95e29e38b65ac0a3
Marcos Martínez <marcos@mx>
2023-02-11 18:50:01 -0300

    Actualización de Componentes

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Contact.css";
 import { AiOutlineMail } from "react-icons/ai";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { useRef } from 'react';
 import emailjs from "emailjs-com";
 
@@ -21,10 +22,22 @@ export const Contact = () => {
             <div className="container contact__container">
                 <div className="contact__options">
                     <article className="contact__option">
-                        <AiOutlineMail />
+                        <AiOutlineMail className="contact__option-icon" />
                         <h4>Correo Electrónico</h4>
                         <h5>marcossebastian429@gmail.com</h5>
                         <a href="mailto:marcossebastian429@gmail.com" target="_blank">Enviar un mensaje</a>
+                    </article>
+                    <article className="contact__option">
+                        <FaLinkedin className="contact__option-icon" />
+                        <h4>LinkedIn</h4>
+                        <h5>Marcos Sebastián Martínez</h5>
+                        <a href="https://www.linkedin.com/in/marcos-sebastian-martinez/" target="_blank">Ver perfil</a>
+                    </article>
+                    <article className="contact__option">
+                        <FaGithub className="contact__option-icon" />
+                        <h4>GitHub</h4>
+                        <h5>MarcosSebastian-Martinez19</h5>
+                        <a href="https://github.com/MarcosSebastian-Martinez19" target="_blank">Ver repositorio</a>
                     </article>
                 </div>
                 
