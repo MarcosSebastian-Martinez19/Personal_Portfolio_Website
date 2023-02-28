d51988c279915b7d340b258e051a3d436807f674
Marcos Martínez <marcos@mx>
2023-02-28 15:10:31 -0300

    Actualización de Interfaz e Información

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import "./Portfolio.css";
 import Proyectos from "./Proyectos.json";
-import { FaGithub } from "react-icons/fa"
-import { SiNetlify } from "react-icons/si"
 import { Search } from "./Search/Search";
+import { FaGithub } from "react-icons/fa";
+import { SiNetlify } from "react-icons/si";
 
 export const Portfolio = () => {
     const [proyectos, setProyectos] = useState(Proyectos);
+    const [activeFilter, setActiveFilter] = useState([]);
+    const [valueSearch, setValueSearch] = useState("");
     const handleSearch = (e) => {
+        setValueSearch(e);
         const q = e.toLowerCase();
-        
-        if(!!q) {
+
+        if (!!q) {
             const search = Proyectos.filter((proyecto) => {
                 return (
                     proyecto.title.toLowerCase().includes(q)
@@ -22,38 +25,58 @@ export const Portfolio = () => {
         }
     }
 
+    function handleFilter(e) {
+        const q = e.target.value;
+
+        if (!!q) {
+            setActiveFilter(q);
+            const filter = Proyectos.filter((proyecto) => {
+                return proyecto.usedTechnology.includes(q)
+            });
+            setProyectos(filter);
+        } else {
+            setProyectos(Proyectos);
+        }
+    }
+
     return (
-    <section id="portfolio">
-        <h2>Portafolio</h2>
-        <Search onSearch={handleSearch} />
-        <div className="container__filtro">
-            <button>Html</button>
-            <button>Css</button>
-            <button>Scss</button>
-            <button>Bootstrap</button>
-            <button>JavaScript</button>
-            <button>TypeScript</button>
-            <button>React JS</button>
-            <button>Angular</button>
-        </div>
-        <div className="container portfolio__container">
+        <section id="portfolio">
+            <h2>Portafolio</h2>
+            <Search onSearch={handleSearch} />
             {
-                proyectos.map(({ id, image, title, urlGitHub, urlLiveDemo }) => {
-                    return (
-                        <article key={id} className="portfolio__item">
-                            <div className="portfolio__item-image">
-                                <img src={image} alt={"Image " + title} />
-                            </div>
-                            <h3>{title}</h3>
-                            <div className="portfolio__item-cta">
-                                <a href={urlGitHub} className="btn" target="_blank"> <FaGithub /> GitHub</a>
-                                <a href={urlLiveDemo} className="btn btn-primary" target="_blank"> <SiNetlify /> Ver Demo</a>
-                            </div>
-                        </article>
-                    )
-                })
+                valueSearch !== "" ? <div className="result__container">
+                    <p>Resultados para: {valueSearch}</p>
+                </div> : valueSearch
             }
-        </div>
-    </section>
-)
+            
+            <div className="container__filtro">
+                <input onClick={handleFilter} className={activeFilter === "Html" ? "active" : ""} type="button" value="Html" />
+                <input onClick={handleFilter} className={activeFilter === "Css" ? "active" : ""} type="button" value="Css" />
+                <input onClick={handleFilter} className={activeFilter === "Scss" ? "active" : ""} type="button" value="Scss" />
+                <input onClick={handleFilter} className={activeFilter === "Bootstrap" ? "active" : ""} type="button" value="Bootstrap" />
+                <input onClick={handleFilter} className={activeFilter === "JavaScript" ? "active" : ""} type="button" value="JavaScript" />
+                <input onClick={handleFilter} className={activeFilter === "TypeScript" ? "active" : ""} type="button" value="TypeScript" />
+                <input onClick={handleFilter} className={activeFilter === "React JS" ? "active" : ""} type="button" value="React JS" />
+                <input onClick={handleFilter} className={activeFilter === "Angular" ? "active" : ""} type="button" value="Angular" />
+            </div>
+            <div className="container portfolio__container">
+                {
+                    proyectos.map(({ id, image, title, urlGitHub, urlLiveDemo }) => {
+                        return (
+                            <article key={id} className="portfolio__item">
+                                <div className="portfolio__item-image">
+                                    <img src={image} alt={"Image " + title} />
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__item-cta">
+                                    <a href={urlGitHub} className="btn" target="_blank"> <FaGithub /> GitHub</a>
+                                    <a href={urlLiveDemo} className="btn btn-primary" target="_blank"> <SiNetlify /> Ver Demo</a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </div>
+        </section>
+    )
 };
